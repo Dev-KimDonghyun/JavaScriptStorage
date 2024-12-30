a6e52ee9b43f6aad8be01fe1f5db3e6a6383be50
Dev-KimDonghyun <hyungus723@gmail.com>
2024-12-30 12:59:25 +0900

    new file

---
+++ S24123003.js
@@ -0,0 +1,14 @@
+console.dir(Navigator);
+// dir을 쓰면 Navigator 목록을 전부 다 볼 수 있음
+
+console.dir(Navigator.appName);
+// 브라우저 이름 출력, 허나 변별력 없음
+
+console.dir(Navigator.appVersion);
+// 브라우저에 대한 여러 정보를 볼 수 있음
+
+console.dir(Navigator.userAgent);
+// 서버에 보내는 브라우저 정보
+
+console.dir(Navigator.platform);
+// 사용자의 운영체제
