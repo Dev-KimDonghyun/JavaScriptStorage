bd4ec3aa6f4e133d2b68af8bb5281f31bc33ca45
Dev-KimDonghyun <hyungus723@gmail.com>
2025-01-02 13:22:34 +0900

    new file

---
+++ 25010201.js
@@ -0,0 +1,14 @@
+window.open('index.html');
+// 창 열기
+
+window.open('index.html', '_self');
+// 현재 창에서 창 열기
+
+window.open('index.html', '_blank');
+// 새 창에서 창 열기
+
+window.open('index.html', 'name');
+// 새 창에서 name창 열기. 다시 name창을 열려는 시도를 하면 열리지 않음
+
+window.open('index.html', '_blank', 'width = 200, height = 200');
+// 보면 알거임
